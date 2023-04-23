7c1372bd33df4c473daa4d595dcbffc7d8f11f8b
ElXreno <elxreno@gmail.com>
2023-04-24 00:36:11 +0300

    Import minimal flake setup

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [ kubectl kubernetes-helm ];
+
+          shellHook = ''
+            SHELL_NAME=$(basename "$SHELL")
+
+            eval $(kubectl completion "$SHELL_NAME")
+          '';
+        };
+      });
+}
